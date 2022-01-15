76c888746a5ced494f07041b3d2209258d3e96ae
shudongzhang <zhangshudong@gmail.com>
2022-01-15 21:28:01 +0800

    update

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,5 +1,5 @@
 import { html, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import { debounce } from 'lodash-es';
 
 console.log(debounce);
@@ -19,23 +19,27 @@ export class MyElement extends LitElement {
       padding: 16px;
       max-width: 800px;
     }
-  `
+  `;
+
+  @state() 
+  private end: number | null = null;
 
   /**
    * The name to say "Hello" to.
    */
-  @property()
-  name = 'World'
+  @property({ attribute: false })
+  name = 'World';
 
   /**
    * The number of times the button has been clicked.
    */
   @property({ type: Number })
-  count = 0
+  count = 0;
 
   render() {
     console.log(import.meta.env);
     console.log('hello3');
+    console.log(this.end);
     return html`
       <h1>Hello, ${this.name}!</h1>
       <button @click=${this._onClick} part="button">
@@ -46,11 +50,11 @@ export class MyElement extends LitElement {
   }
 
   private _onClick() {
-    this.count++
+    this.count++;
   }
 
   foo(): string {
-    return 'foo'
+    return 'foo';
   }
 }
 
